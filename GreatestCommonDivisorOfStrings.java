5aee89d08fa1fb92282b10b08ab1694c512e1594
geezylucas <sorec@live.com>
2025-03-24 22:43:39 -0600

    add new code greatest common divisor of string

--- GreatestCommonDivisorOfStrings.java
+++ GreatestCommonDivisorOfStrings.java
@@ -1,16 +1,21 @@
 public class GreatestCommonDivisorOfStrings {
 
-    public static String gcdOfStrings(String str1, String str2) {
-        // Check if concatenated strings are equal or not, if not return ""
-        if (!(str1 + str2).equals(str2 + str1))
+    public String gcdOfStrings(String str1, String str2) {
+        if (!(str1 + str2).equals(str2 + str1)) {
             return "";
-        // If strings are equal than return the substring from 0 to gcd of size(str1),
-        // size(str2)
-        int gcd = gcd(str1.length(), str2.length());
-        return str1.substring(0, gcd);
+        }
+
+        int i = gcd(str1.length(), str2.length());
+
+        return str1.substring(0, i);
     }
 
-    private static int gcd(int a, int b) {
-        return b == 0 ? a : gcd(b, a % b);
+    // Euclidean algorithm (repeated subtraction)
+    public int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+
+        return gcd(b, Math.abs(a - b));
     }
 }
