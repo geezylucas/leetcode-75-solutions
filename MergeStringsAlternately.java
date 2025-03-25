bed012b775ab52cf475a9c20191ee4305e7450f1
geezylucas <sorec@live.com>
2025-03-24 22:43:39 -0600

    Update MergeStringsAlternately

--- MergeStringsAlternately.java
+++ MergeStringsAlternately.java
@@ -1,16 +1,29 @@
+/**
+ * Using two-pointer technique for the solution
+ */
 public class MergeStringsAlternately {
 
     public String mergeAlternately(String word1, String word2) {
         int i = 0;
+        int j = 0;
+
         StringBuilder newWord = new StringBuilder();
-        while (i < word1.length() || i < word2.length()) {
-            if (i < word1.length()) {
-                newWord.append(word1.charAt(i));
-            }
-            if (i < word2.length()) {
-                newWord.append(word2.charAt(i));
-            }
+        while (i < word1.length() && j < word2.length()) {
+            newWord.append(word1.charAt(i));
+            newWord.append(word2.charAt(j));
+
             i++;
+            j++;
+        }
+
+        while (i < word1.length()) {
+            newWord.append(word1.charAt(i));
+            i++;
+        }
+
+        while (j < word2.length()) {
+            newWord.append(word2.charAt(j));
+            j++;
         }
 
         return newWord.toString();
