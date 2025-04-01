6ab88e0ba26b2f47d3c930d27e804837fdd3308b
geezylucas <sorec@live.com>
2025-04-01 13:38:13 -0600

    feat: repeat KidsWithTheGreatestNumberOfCandies

--- KidsWithTheGreatestNumberOfCandies.java
+++ KidsWithTheGreatestNumberOfCandies.java
@@ -4,17 +4,17 @@ import java.util.List;
 public class KidsWithTheGreatestNumberOfCandies {
 
     public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        // first need to find the maximum in candies[i] and save result
         int max = candies[0];
 
-        for (int candy : candies) {
-            max = Math.max(max, candy);
+        for (int i = 1; i < candies.length; i++) {
+            max = Math.max(max, candies[i]);
         }
 
+        // then need to evaluate if after given extraCandies, kid has the greatest number of candies among all the kids and save result in ArrayList
         List<Boolean> result = new ArrayList<>();
         for (int i = 0; i < candies.length; i++) {
-            int sum = candies[i] + extraCandies;
-
-            if (sum >= max) {
+            if (candies[i] + extraCandies >= max ) {
                 result.add(true);
             } else {
                 result.add(false);
